ce8e165bd38728970d3df367ad22910766e98b6e
Shashank Viswanadha <sashankviswanadha@gmail.com>
2020-04-16 19:17:00 -0400

    Renaming file

--- src/test/java/com/newsstand/NewsstandTests.java
+++ src/test/java/com/newsstand/NewsstandTests.java
@@ -1,4 +1,4 @@
-//package com.moviehub;
+//package com.newsstand;
 //
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@
 //
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
-//public class MoviehubServerApplicationTests {
+//public class NewsstandTests {
 //
 //	@Test
 //	public void contextLoads() {
